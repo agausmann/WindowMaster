d7a66b72d0bc99e7123b3dbf2f5be37f18236d07
Adam Gausmann <agausmann@fastmail.com>
2021-03-12 03:31:31 -0600

    Rough PoC of audio device/app enumeration on Windows

---
+++ host-controller/build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    windows::build!(
+        windows::win32::audio::IPropertyStore,
+        windows::win32::core_audio::{
+            EDataFlow, IAudioSessionControl2, IAudioSessionEnumerator, IAudioSessionManager2,
+            IMMDevice, IMMDeviceCollection, IMMDeviceEnumerator, ISimpleAudioVolume,
+            MMDeviceEnumerator,
+        },
+        windows::win32::process_status::K32GetModuleBaseNameW,
+        windows::win32::structured_storage::PROPVARIANT,
+        windows::win32::system_services::{OpenProcess, BOOL, GW_OWNER, PROCESS_ACCESS_RIGHTS, PWSTR},
+        windows::win32::windows_programming::CloseHandle,
+        windows::win32::windows_properties_system::PROPERTYKEY,
+    );
+}
